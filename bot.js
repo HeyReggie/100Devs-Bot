5861c10a97ced4dcebed64669cc5470901185b02
Reginald Traywick <reggie@hey.com>
2021-10-03 11:56:41 -0700

    working search and retweets

---
+++ bot.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const Twitter = require("twitter-v2");
+
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_KEY_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+const retweet = async () => {
+  const params = {
+    query: "#100Devs -is:retweet -is:quote",
+  };
+
+  const {
+    data: tweets,
+    meta,
+    errors,
+  } = await client.get("tweets/search/recent", params);
+
+  const retweetId = tweets[Math.floor(Math.random() * 2)].id;
+
+  //console.log(retweetId);
+
+  const response = await client.post(`users/${process.env.USER_ID}/retweets`, {
+    tweet_id: retweetId,
+  });
+
+  if (errors) {
+    console.log("Errors:", errors);
+    return;
+  }
+
+  console.log(response);
+  //console.log(tweets);
+  console.log(meta);
+};
+
+retweet();
+
+setInterval(retweet, 60000);
